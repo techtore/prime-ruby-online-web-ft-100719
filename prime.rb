a3a270ac8364feaaa37756a2c0339ec4ad0437c2
Victoria Hale <vic.hale74@gmail.com>
2019-10-11 16:41:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,5 +1,6 @@
 def prime?(number)
 return false if number < 2 #checks for -integer, 1 and 0
 
-(2..number/2).none?{|num| number % num == 0} #checking integer against rang of numbers to see if number can be divided by any number in the range. if it can't return is true
+(2..number/2).none?{|num| number % num == 0} 
+#checking integer against range of numbers to see if number can be divided by any number in the range. if it can't return is true
 end
